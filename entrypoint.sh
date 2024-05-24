2866f61a49cb686fb2fe4a9a801c17136f890aeb
Aurelien Roy <royaurelien@users.noreply.github.com>
2024-05-24 11:44:36 +0200

    [IMP] Read odoo version from file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-WORKDIR=/github/workspace
-ADDONS_PATH=/mnt/extra-addons
-BASE=$1
+WORKDIR = /github/workspace
+ADDONS_PATH = /mnt/extra-addons
 
-PKG_FILE="${WORKDIR}/packages.txt"
-REQ_FILE="${WORKDIR}/requirements.txt"
+PKG_FILE = "${WORKDIR}/packages.txt"
+REQ_FILE = "${WORKDIR}/requirements.txt"
+VERSION_FILE = "${WORKDIR}/$1"
+BASE = "`cat ${VERSION_FILE}`"
 
 if [ -f ${PKG_FILE} ]
 then
